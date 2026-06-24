7e82f636700cd49866a60e0a534e2e3648317c89
Reyy-btv1 <reyymusicaja@gmail.com>
2026-06-24 23:04:53 +0700

    Change package name to rvpyro and update author metadata

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -17,10 +17,10 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
-__fork_name__ = "PyroFork"
+__fork_name__ = "rvpyro"
 __version__ = "2.3.69"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
-__copyright__ = "Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>"
+__copyright__ = "Copyright (C) 2026-present rvpyro"
 
 from concurrent.futures.thread import ThreadPoolExecutor
 
