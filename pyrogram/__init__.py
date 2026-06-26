f128d13f039c19325ee506c258e40f53adaff7d8
Reyy-btv1 <reyymusicaja@gmail.com>
2026-06-26 20:06:16 +0700

    chore: bump version to 2.0.200

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -18,7 +18,7 @@
 #  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
 __fork_name__ = "rvpyro"
-__version__ = "2.3.69"
+__version__ = "2.0.200"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2026-present rvpyro"
 
